eac7447376cff22e937cb40f6c2924bb75170eb1
markxoe <mark@oude-elberink.de>
2020-10-20 17:31:01 +0200

    :sparkles: Biene rotation

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,20 +1,25 @@
 import {
   IonButton,
   IonButtons,
-  IonCard,
-  IonCardContent,
+  IonCol,
   IonContent,
+  IonGrid,
   IonHeader,
   IonIcon,
   IonPage,
+  IonRow,
   IonTitle,
   IonToolbar,
 } from "@ionic/react";
-import React from "react";
+import React, { useState } from "react";
 import { settingsOutline } from "ionicons/icons";
 import "./Home.css";
 
+import biene from "../res/biene.png";
+
 const Home: React.FC = () => {
+  const [rotateBee, setRotateBee] = useState<boolean>(false);
+
   return (
     <IonPage>
       <IonHeader>
@@ -38,13 +43,19 @@ const Home: React.FC = () => {
             </IonButtons>
           </IonToolbar>
         </IonHeader>
-        <IonCard>
-          <IonCardContent>
-            <span aria-label="Biene" role="img" id="biene">
-              🐝
-            </span>
-          </IonCardContent>
-        </IonCard>
+        <IonGrid className="ion-margin-top">
+          <IonRow>
+            <IonCol>
+              <div className="ion-text-center">
+                <img
+                  onClick={() => setRotateBee((e) => !e)}
+                  className={rotateBee ? "bienerotate" : "biene"}
+                  src={biene}
+                />
+              </div>
+            </IonCol>
+          </IonRow>
+        </IonGrid>
       </IonContent>
     </IonPage>
   );
