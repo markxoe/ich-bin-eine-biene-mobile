d48352943aafba3d740826c30e1dbb78d9c5796c
markxoe <mark@oude-elberink.de>
2020-11-13 15:09:26 +0100

    :bug: Bug Fix, Floating Point to Int

--- src/globals.tsx
+++ src/globals.tsx
@@ -1,22 +1,26 @@
-export const rotateSpeedLevelClasses = [".bienerotate",".bienerotate-level-2",".bienerotate-level-3"];
+export const rotateSpeedLevelClasses = [
+  ".bienerotate",
+  ".bienerotate-level-2",
+  ".bienerotate-level-3",
+];
 export const rotateSpeedLevelMax = 3;
 
-export const getRotateSpeedLevelPrice = (level:number) => {
-  return (level*200)+100;
-}
 export const rotateSpeedLevel = {
-  max:2,
-  levels:[
-    {class:"bienerotate"},
-    {class:"bienerotate-level-2"},
-    {class:"bienerotate-level-3"}
-  ]
-}
+  max: 2,
+  levels: [
+    { class: "bienerotate" },
+    { class: "bienerotate-level-2" },
+    { class: "bienerotate-level-3" },
+  ],
+};
 
 export const BeePrices = {
-  speedLevel:20
-}
+  speedLevel: 20,
+};
 
-export const getAdditionalBeePrice = (level:number) => {
-  return (level*(level*0.2))*100+200;
-}
+export const getRotateSpeedLevelPrice = (level: number): number => {
+  return Math.round(level * 200 + 100);
+};
+export const getAdditionalBeePrice = (level: number): number => {
+  return Math.round(level * (level * 0.2) * 100 + 200);
+};
