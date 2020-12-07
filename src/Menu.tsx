dc5c70783c1d7ac3f7d37d407a4e0c3bc19dc67e
markxoe <mark@oude-elberink.de>
2020-12-07 16:49:40 +0100

    :lipstick: Menu UI Improvements

--- src/Menu.tsx
+++ src/Menu.tsx
@@ -8,6 +8,7 @@ import {
   IonItem,
   IonLabel,
   IonList,
+  IonListHeader,
   IonMenu,
   IonMenuToggle,
   IonTitle,
@@ -27,11 +28,11 @@ import { AppContext } from "./store/State";
 const Menu: React.FC = () => {
   const { state } = useContext(AppContext);
   return (
-    <IonMenu contentId="main" type="overlay">
+    <IonMenu contentId="main">
       <IonHeader>
         <IonToolbar>
           <IonTitle>Ich bin ein Menü</IonTitle>
-          <IonButtons hidden slot="end">
+          <IonButtons collapse hidden slot="end">
             <IonButton>
               <IonIcon icon={moon} />
             </IonButton>
@@ -39,7 +40,19 @@ const Menu: React.FC = () => {
         </IonToolbar>
       </IonHeader>
       <IonContent>
+        <IonHeader collapse="condense">
+          <IonToolbar>
+            <IonTitle>Ich bin ein Menü</IonTitle>
+            <IonButtons hidden slot="end">
+              <IonButton>
+                <IonIcon icon={moon} />
+              </IonButton>
+            </IonButtons>
+          </IonToolbar>
+        </IonHeader>
+
         <IonList lines={"none"}>
+          <IonListHeader>Allgemein</IonListHeader>
           <IonMenuToggle>
             <IonItem routerLink="/home">
               <IonIcon icon={home} slot="start" />
@@ -52,11 +65,7 @@ const Menu: React.FC = () => {
               Store
             </IonItem>
           </IonMenuToggle>
-          <IonMenuToggle>
-            <IonItem routerLink="/infos">
-              <IonIcon slot="start" icon={help} /> Informationen
-            </IonItem>
-          </IonMenuToggle>
+          <IonListHeader>Benutzer</IonListHeader>
           <IonMenuToggle>
             <IonItem routerLink="/leader">
               <IonIcon slot="start" icon={trophy} />
@@ -74,10 +83,16 @@ const Menu: React.FC = () => {
               </IonLabel>
             </IonItem>
           </IonMenuToggle>
+          <IonListHeader>Weiteres</IonListHeader>
           <IonMenuToggle>
             <IonItem routerLink="/settings">
               <IonIcon slot="start" icon={settings} />
               Einstellungen
+            </IonItem>
+          </IonMenuToggle>
+          <IonMenuToggle>
+            <IonItem routerLink="/infos">
+              <IonIcon slot="start" icon={help} /> Informationen
             </IonItem>
           </IonMenuToggle>
         </IonList>
