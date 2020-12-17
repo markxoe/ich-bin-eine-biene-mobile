b5d06647643a30dc188eaabcc2656eaed18c872a
markxoe <mark@oude-elberink.de>
2020-12-17 17:34:54 +0100

    :children_crossing: It gets a lil complicater with each purchase

--- src/globals.tsx
+++ src/globals.tsx
@@ -21,7 +21,13 @@ export const getAdditionalBeePrice = (state: stateType): number => {
       80 *
       (state.biene.multiplierLevel +
         1 +
-        state.biene.autoRotatingBees.length * 0.5)
+        state.biene.autoRotatingBees.length * 0.5) *
+      Math.pow(
+        1.002,
+        state.biene.additionalBienen.length +
+          state.biene.multiplierLevel +
+          state.biene.autoRotatingBees.length
+      )
   );
 };
 export const getMultiplierPrice = (state: stateType): number => {
