d3b48d7377cd2c32b6e5368e90c5e9a6862a98cb
markxoe <mark@oude-elberink.de>
2021-01-15 12:21:36 +0100

    :sparkles: Infinity: Change Values

--- src/other/const.ts
+++ src/other/const.ts
@@ -1,4 +1,7 @@
 export const oldStoreKeyPrefix = "ichbineinebiene.";
 export const StoreKeyPrefix = "ichbineinebiene2.";
 
-export const MAX_VALUE = 9007199254700000;
+export const MAX_VALUE = 69420000000000;
+export const MAX_ARRAY_LENGTH = 10000;
+
+// 69.420.000.000.000
