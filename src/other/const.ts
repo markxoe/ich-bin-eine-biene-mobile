d0746504c5b443de357f109436f6f13b920ce020
markxoe <mark@oude-elberink.de>
2021-01-18 10:36:02 +0100

    :sparkles: Changed max array size

--- src/other/const.ts
+++ src/other/const.ts
@@ -2,6 +2,6 @@ export const oldStoreKeyPrefix = "ichbineinebiene.";
 export const StoreKeyPrefix = "ichbineinebiene2.";
 
 export const MAX_VALUE = 69420000000000;
-export const MAX_ARRAY_LENGTH = 10000;
+export const MAX_ARRAY_LENGTH = 500000;
 
 // 69.420.000.000.000
