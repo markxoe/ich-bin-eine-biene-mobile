86f322e820fe622f338c04ff76fb4eb905d68343
markxoe <mark@oude-elberink.de>
2021-02-12 15:32:24 +0100

    :construction_worker: Adding Test for Release Notes

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,7 +2,13 @@ import React from "react";
 import { render } from "@testing-library/react";
 import App from "./App";
 
+import packageJSON from "../package.json";
+import releaseNotesJSON from "./other/release-notes.json";
+
 test("renders without crashing", () => {
   const { baseElement } = render(<App />);
   expect(baseElement).toBeDefined();
+});
+test("Release Notes Header includes latest Version", () => {
+  expect(releaseNotesJSON.header).toContain(packageJSON.version);
 });
